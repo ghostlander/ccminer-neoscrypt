9fac109eb7816069bbe1b0f165071d1e5b91d040
sp-hash <sp-hash@users.noreply.github.com>
2014-11-16 18:23:27 +0100

    Faster Blake

--- quark/cuda_quark_blake512.cu
+++ quark/cuda_quark_blake512.cu
@@ -60,10 +60,52 @@ const uint64_t c_u512[16] =
 	v[b] = ROTR( v[b] ^ v[c], 11); \
 }
 
-__device__ __forceinline__ 
-void quark_blake512_compress( uint64_t *h, const uint64_t *block, const uint8_t ((*sigma)[16]), const uint64_t *u512, const int T0)
+__device__ __forceinline__
+void G3(uint64_t a, uint64_t b, uint64_t c, uint64_t d, const uint64_t x, uint64_t *m, uint64_t *v, int i)
 {
-    uint64_t v[16], m[16], i;
+	const uint32_t	idx1 = c_sigma[i][x];
+	const uint32_t	idx2 = c_sigma[i][x + 1];
+	const uint64_t const1 = m[idx1];
+	const uint64_t const2 = c_u512[idx2];
+	const uint64_t const3 = m[idx2];
+	const uint64_t const4 = c_u512[idx1];
+
+	uint64_t		t;
+	uint2			t3, result;
+	short4			temp;
+	//	short4			t4;
+	asm("xor.b64	%0, %1, %2;" : "=l"(t) : "l"(const1), "l"(const2));
+	asm("add.s64 	%0, %1, %2;" : "=l"(v[a]) : "l"(v[a]), "l"(v[b]));
+	asm("add.s64 	%0, %1, %2;" : "=l"(v[a]) : "l"(v[a]), "l"(t));
+	asm("xor.b64 	%0, %1, %2;" : "=l"(t) : "l"(v[d]), "l"(v[a]));
+	asm("mov.b64 	{%0, %1}, %2 ;" : "=r"(t3.x), "=r"(t3.y) : "l"(t));
+	asm("mov.b64 	%0, {%1, %2} ;" : "=l"(v[d]) : "r"(t3.y), "r"(t3.x));
+	asm("add.s64 	%0, %1, %2;" : "=l"(v[c]) : "l"(v[c]), "l"(v[d]));
+	asm("xor.b64 	%0, %1, %2;" : "=l"(t) : "l"(v[b]), "l"(v[c]));
+	asm("mov.b64 	{%0, %1}, %2 ;" : "=r"(t3.x), "=r"(t3.y) : "l"(t));
+	asm("shf.r.wrap.b32 %0, %1, %2, 25;" : "=r"(result.x) : "r"(t3.y), "r"(t3.x));
+	asm("shf.r.wrap.b32 %0, %1, %2, 25;" : "=r"(result.y) : "r"(t3.x), "r"(t3.y));
+	asm("mov.b64 	%0, {%1, %2} ;" : "=l"(v[b]) : "r"(result.y), "r"(result.x));
+	asm("xor.b64 	%0, %1, %2;" : "=l"(t) : "l"(const3), "l"(const4));
+	asm("add.s64 	%0, %1, %2;" : "=l"(t) : "l"(v[b]), "l"(t));
+	asm("add.s64 	%0, %1, %2;" : "=l"(v[a]) : "l"(v[a]), "l"(t));
+	asm("xor.b64  	%0, %1, %2;" : "=l"(v[d]) : "l"(v[d]), "l"(v[a]));
+	asm("mov.b64 { %0,  %1, %2, %3 }, %4; ": "=h"(temp.x), "=h"(temp.y), "=h"(temp.z), "=h"(temp.w) : "l"(v[d]));
+	asm("mov.b64 %0, {%1, %2, %3 , %4}; ":  "=l"(v[d]) : "h"(temp.y), "h"(temp.z), "h"(temp.w), "h"(temp.x));
+	asm("add.s64 	%0, %1, %2;" : "=l"(v[c]) : "l"(v[c]), "l"(v[d]));
+	asm("xor.b64  	%0, %1, %2;" : "=l"(v[b]) : "l"(v[b]), "l"(v[c]));
+	asm("mov.b64 	{%0, %1}, %2 ;" : "=r"(t3.x), "=r"(t3.y) : "l"(v[b]));
+	asm("shf.r.wrap.b32 %0, %1, %2, 11;" : "=r"(result.x) : "r"(t3.y), "r"(t3.x));
+	asm("shf.r.wrap.b32 %0, %1, %2, 11;" : "=r"(result.y) : "r"(t3.x), "r"(t3.y));
+	asm("mov.b64 	%0, {%1, %2} ;" : "=l"(v[b]) : "r"(result.y), "r"(result.x));
+}
+
+__device__ __forceinline__ 
+void quark_blake512_compress(uint64_t *h, const uint64_t *block, const uint8_t((*sigma)[16]), const uint64_t *u512, const int T0)
+{
+	uint64_t v[16];
+	uint64_t m[16];
+	int i;
 
 	#pragma unroll 16
 	for( i = 0; i < 16; i++) {
@@ -73,7 +115,6 @@ void quark_blake512_compress( uint64_t *h, const uint64_t *block, const uint8_t 
 	#pragma unroll 8
 	for (i = 0; i < 8; i++)
 		v[i] = h[i];
-
 	v[ 8] = u512[0];
 	v[ 9] = u512[1];
 	v[10] = u512[2];
@@ -86,22 +127,38 @@ void quark_blake512_compress( uint64_t *h, const uint64_t *block, const uint8_t 
 	//#pragma unroll 16
 	for( i = 0; i < 16; ++i )
 	{
-		/* column step */
+
+#if __CUDA_ARCH__ < 520
 		G( 0, 4, 8, 12, 0 );
 		G( 1, 5, 9, 13, 2 );
 		G( 2, 6, 10, 14, 4 );
 		G( 3, 7, 11, 15, 6 );
-		/* diagonal step */
 		G( 0, 5, 10, 15, 8 );
 		G( 1, 6, 11, 12, 10 );
 		G( 2, 7, 8, 13, 12 );
 		G( 3, 4, 9, 14, 14 );
+ #else
+		G3(0, 4, 8, 12, 0,m,v,i);
+		G3(1, 5, 9, 13, 2, m, v, i);
+		G3(2, 6, 10, 14, 4, m, v, i);
+		G3(3, 7, 11, 15, 6, m, v, i);
+		G3(0, 5, 10, 15, 8, m, v, i);
+		G3(1, 6, 11, 12, 10, m, v, i);
+		G3(2, 7, 8, 13, 12, m, v, i);
+		G3(3, 4, 9, 14, 14, m, v, i);
+#endif
 	}
 
-	#pragma unroll 16
-	for( i = 0; i < 16; ++i )
-		h[i % 8] ^= v[i];
+	h[0] ^= v[0] ^ v[8];
+	h[1] ^= v[1] ^ v[9];
+	h[2] ^= v[2] ^ v[10];
+	h[3] ^= v[3] ^ v[11];
+	h[4] ^= v[4] ^ v[12];
+	h[5] ^= v[5] ^ v[13];
+	h[6] ^= v[6] ^ v[14];
+	h[7] ^= v[7] ^ v[15];
 }
+
 
 // Hash-Padding
 __device__ __constant__
@@ -116,7 +173,12 @@ static const uint64_t d_constHashPadding[8] = {
 	0x0002000000000000ull
 };
 
-__global__ __launch_bounds__(256, 4)
+__global__ 
+#if __CUDA_ARCH__ > 500
+	__launch_bounds__(256, 2)
+#else
+	__launch_bounds__(256, 4)
+#endif
 void quark_blake512_gpu_hash_64(int threads, uint32_t startNounce, uint32_t *g_nonceVector, uint64_t *g_hash)
 {
 	int thread = (blockDim.x * blockIdx.x + threadIdx.x);
@@ -181,7 +243,12 @@ void quark_blake512_gpu_hash_64(int threads, uint32_t startNounce, uint32_t *g_n
 	}
 }
 
-__global__ __launch_bounds__(256,1)
+__global__ 
+#if __CUDA_ARCH__ > 500
+__launch_bounds__(256, 2)
+#else
+__launch_bounds__(256, 4)
+#endif
 void quark_blake512_gpu_hash_80(int threads, uint32_t startNounce, void *outputHash)
 {
 	int thread = (blockDim.x * blockIdx.x + threadIdx.x);
@@ -259,10 +326,10 @@ __host__ void quark_blake512_cpu_setBlock_80(void *pdata)
 	);
 }
 
+
 __host__ void quark_blake512_cpu_hash_64(int thr_id, int threads, uint32_t startNounce, uint32_t *d_nonceVector, uint32_t *d_outputHash, int order)
 {
 	const int threadsperblock = 256;
-
 	// berechne wie viele Thread Blocks wir brauchen
 	dim3 grid((threads + threadsperblock-1)/threadsperblock);
 	dim3 block(threadsperblock);
@@ -279,7 +346,6 @@ __host__ void quark_blake512_cpu_hash_64(int thr_id, int threads, uint32_t start
 __host__ void quark_blake512_cpu_hash_80(int thr_id, int threads, uint32_t startNounce, uint32_t *d_outputHash, int order)
 {
 	const int threadsperblock = 256;
-
 	// berechne wie viele Thread Blocks wir brauchen
 	dim3 grid((threads + threadsperblock-1)/threadsperblock);
 	dim3 block(threadsperblock);
