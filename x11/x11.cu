6343487b7b826c32af7664fe748ff0a9f9e790b1
KlausT <kto@gmx.de>
2014-12-04 23:24:30 +0100

    Revert "x11: reduce validation error messages"
    This reverts commit e846bd2db91914b38f4db9a07bcea55598e89ac2.

--- x11/x11.cu
+++ x11/x11.cu
@@ -216,8 +216,7 @@ extern "C" int scanhash_x11(int thr_id, uint32_t *pdata,
 				applog(LOG_INFO, "GPU #%d: result for %08x is not in range: %x > %x", thr_id, foundNonce, vhash64[7], Htarg);
 			}
 			else {
-error:			if (vhash64[7] != Htarg)
-					applog(LOG_INFO, "GPU #%d: result for %08x does not validate on CPU!", thr_id, foundNonce);
+error:			applog(LOG_INFO, "GPU #%d: result for %08x does not validate on CPU!", thr_id, foundNonce);
 			}
 		}
 		pdata[19] += throughput;
