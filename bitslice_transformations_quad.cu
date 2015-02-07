2a1b8c243c0fe64c6748dd7bb060ddaa9dd02b02
sp-hash <rune.stensland@bradyplc.com>
2015-02-07 21:33:28 +0100

    faster groestl

--- bitslice_transformations_quad.cu
+++ bitslice_transformations_quad.cu
@@ -10,6 +10,10 @@
 #define merge8(z,x,y)\
 		z=__byte_perm(x, y, 0x5140); \
 
+#define merge8_swap16(z,x,y)\
+		z=__byte_perm(x, y, 0x7362); \
+
+
 #define SWAP8(x,y)\
 		x=__byte_perm(x, y, 0x5410); \
 		y=__byte_perm(x, y, 0x7632);
@@ -45,48 +49,78 @@ void to_bitslice_quad(uint32_t *const __restrict__ input, uint32_t *const __rest
     uint32_t other[8];
 	uint32_t t;
 
-	uint32_t t1 = threadIdx.x & 1;
-	uint32_t t2 = threadIdx.x & 2;
-	uint32_t t3 = (threadIdx.x + 1) & 3;
-	const unsigned int n = threadIdx.x & 3;
-
-    #pragma unroll
-    for (int i = 0; i < 8; i++) 
+	if (threadIdx.x & 1)
 	{
-		input[i] = __shfl((int)input[i], n ^ (3 * (n >= 1 && n <= 2)), 4);
-        other[i] = __shfl((int)input[i], t3, 4);
-        input[i] = __shfl((int)input[i], t2, 4);
-        other[i] = __shfl((int)other[i], t2, 4);
-        if (t1) 
+
+#pragma unroll
+		for (int i = 0; i < 8; i++)
 		{
-           input[i] = __byte_perm(input[i], 0, 0x1032);
-           other[i] = __byte_perm(other[i], 0, 0x1032);
-        } 
-    }
+			const unsigned int n = threadIdx.x & 3;
+			input[i] = __shfl((int)input[i], n ^ (3 * (n >= 1 && n <= 2)), 4);
+			other[i] = __shfl((int)input[i], (threadIdx.x + 1) & 3, 4);
+			input[i] = __shfl((int)input[i], threadIdx.x & 2, 4);
+			other[i] = __shfl((int)other[i], threadIdx.x & 2, 4);
+		}
 
-	merge8(output[0], input[0], input[4]);
-	merge8(output[1], other[0], other[4]);
-	merge8(output[2], input[1], input[5]);
-	merge8(output[3], other[1], other[5]);
-	merge8(output[4], input[2], input[6]);
-	merge8(output[5], other[2], other[6]);
-	merge8(output[6], input[3], input[7]);
-	merge8(output[7], other[3], other[7]);
+		merge8_swap16(output[0], input[0], input[4]);
+		merge8_swap16(output[1], other[0], other[4]);
+		merge8_swap16(output[2], input[1], input[5]);
+		merge8_swap16(output[3], other[1], other[5]);
+		merge8_swap16(output[4], input[2], input[6]);
+		merge8_swap16(output[5], other[2], other[6]);
+		merge8_swap16(output[6], input[3], input[7]);
+		merge8_swap16(output[7], other[3], other[7]);
 
-	SWAP1(output[0], output[1]);
-	SWAP1(output[2], output[3]);
-	SWAP1(output[4], output[5]);
-	SWAP1(output[6], output[7]);
+		SWAP1(output[0], output[1]);
+		SWAP1(output[2], output[3]);
+		SWAP1(output[4], output[5]);
+		SWAP1(output[6], output[7]);
 
-	SWAP2(output[0], output[2]);
-	SWAP2(output[1], output[3]);
-	SWAP2(output[4], output[6]);
-	SWAP2(output[5], output[7]);
+		SWAP2(output[0], output[2]);
+		SWAP2(output[1], output[3]);
+		SWAP2(output[4], output[6]);
+		SWAP2(output[5], output[7]);
 
-	SWAP4(output[0], output[4]);
-	SWAP4(output[1], output[5]);
-	SWAP4(output[2], output[6]);
-	SWAP4(output[3], output[7]);
+		SWAP4(output[0], output[4]);
+		SWAP4(output[1], output[5]);
+		SWAP4(output[2], output[6]);
+		SWAP4(output[3], output[7]);
+	}
+	else
+	{
+#pragma unroll
+		for (int i = 0; i < 8; i++)
+		{
+			const unsigned int n = threadIdx.x & 3;
+			input[i] = __shfl((int)input[i], n ^ (3 * (n >= 1 && n <= 2)), 4);
+			other[i] = __shfl((int)input[i], (threadIdx.x + 1) & 3, 4);
+			input[i] = __shfl((int)input[i], threadIdx.x & 2, 4);
+			other[i] = __shfl((int)other[i], threadIdx.x & 2, 4);
+		}
+		merge8(output[0], input[0], input[4]);
+		merge8(output[1], other[0], other[4]);
+		merge8(output[2], input[1], input[5]);
+		merge8(output[3], other[1], other[5]);
+		merge8(output[4], input[2], input[6]);
+		merge8(output[5], other[2], other[6]);
+		merge8(output[6], input[3], input[7]);
+		merge8(output[7], other[3], other[7]);
+
+		SWAP1(output[0], output[1]);
+		SWAP1(output[2], output[3]);
+		SWAP1(output[4], output[5]);
+		SWAP1(output[6], output[7]);
+
+		SWAP2(output[0], output[2]);
+		SWAP2(output[1], output[3]);
+		SWAP2(output[4], output[6]);
+		SWAP2(output[5], output[7]);
+
+		SWAP4(output[0], output[4]);
+		SWAP4(output[1], output[5]);
+		SWAP4(output[2], output[6]);
+		SWAP4(output[3], output[7]);
+	}
 }
 
 __device__ __forceinline__
@@ -122,7 +156,7 @@ void from_bitslice_quad(const uint32_t *const __restrict__ input, uint32_t *cons
 	output[12] = output[8];
 	output[14] = output[10];
 
-/*	if (threadIdx.x & 1) 
+	if (threadIdx.x & 1) 
 	{
 		output[0] = __byte_perm(output[0], 0, 0x1032);
 		output[2] = __byte_perm(output[2], 0, 0x1032);
@@ -133,7 +167,7 @@ void from_bitslice_quad(const uint32_t *const __restrict__ input, uint32_t *cons
 		output[12] = __byte_perm(output[12], 0, 0x3232);
 		output[14] = __byte_perm(output[14], 0, 0x3232);
 	}
-*/
+
 	output[0] = __byte_perm(output[0], __shfl((int)output[0], (threadIdx.x + 1) & 3, 4), 0x7610);
 	output[0 + 1] = __shfl((int)output[0], (threadIdx.x + 2) & 3, 4);
 
