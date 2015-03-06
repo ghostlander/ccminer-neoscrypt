15e2627c4f5ddc729ff0d4546d2d102215b25215
sp-hash <rune.stensland@bradyplc.com>
2015-03-06 15:58:47 +0100

    Keccak + 5MHASH on the 750ti

--- Algo256/cuda_keccak256.cu
+++ Algo256/cuda_keccak256.cu
@@ -43,7 +43,7 @@ __constant__ uint2 keccak_round_constants35[24] = {
 };
 
 
-__constant__ uint64_t c_PaddedMessage80[10]; // padded message (80 bytes + padding?)
+__constant__ uint2 c_PaddedMessage80[10]; // padded message (80 bytes + padding?)
 
 #if __CUDA_ARCH__ >= 350
 __device__ __forceinline__
@@ -293,13 +293,22 @@ static void keccak_blockv30_32(uint64_t *s, const uint64_t *keccak_round_constan
 
 #define bitselect(a, b, c) ((a) ^ ((c) & ((b) ^ (a))))
 
-#if __CUDA_ARCH__ >= 350
-__device__ __forceinline__ 
-static void keccak_blockv35_80(uint2 *s)
+__global__ __launch_bounds__(128,5)
+void keccak256_gpu_hash_80(uint32_t threads, uint32_t startNounce, void *outputHash, uint32_t *resNounce)
 {
-	#pragma unroll 3
-	for (int i = 0; i < 23; i++) {
+	uint32_t thread = (blockDim.x * blockIdx.x + threadIdx.x);
+	if (thread < threads)
+	{
+		uint32_t nounce = startNounce + thread;
 		uint2 bc[5], tmpxor[5], tmp1, tmp2;
+
+		uint2 s[25]=
+		{
+			c_PaddedMessage80[0], c_PaddedMessage80[1], c_PaddedMessage80[2], c_PaddedMessage80[3],
+			c_PaddedMessage80[4], c_PaddedMessage80[5], c_PaddedMessage80[6], c_PaddedMessage80[7],
+			c_PaddedMessage80[8], { c_PaddedMessage80[9].x, cuda_swab32(nounce) }, { 1, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 },
+			{ 0, 0x80000000 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }
+		};
 
 #pragma unroll
 		for (uint32_t x = 0; x < 5; x++)
@@ -344,208 +353,77 @@ static void keccak_blockv35_80(uint2 *s)
 		tmp1 = s[10]; tmp2 = s[11]; s[10] = bitselect(s[10] ^ s[12], s[10], s[11]); s[11] = bitselect(s[11] ^ s[13], s[11], s[12]); s[12] = bitselect(s[12] ^ s[14], s[12], s[13]); s[13] = bitselect(s[13] ^ tmp1, s[13], s[14]); s[14] = bitselect(s[14] ^ tmp2, s[14], tmp1);
 		tmp1 = s[15]; tmp2 = s[16]; s[15] = bitselect(s[15] ^ s[17], s[15], s[16]); s[16] = bitselect(s[16] ^ s[18], s[16], s[17]); s[17] = bitselect(s[17] ^ s[19], s[17], s[18]); s[18] = bitselect(s[18] ^ tmp1, s[18], s[19]); s[19] = bitselect(s[19] ^ tmp2, s[19], tmp1);
 		tmp1 = s[20]; tmp2 = s[21]; s[20] = bitselect(s[20] ^ s[22], s[20], s[21]); s[21] = bitselect(s[21] ^ s[23], s[21], s[22]); s[22] = bitselect(s[22] ^ s[24], s[22], s[23]); s[23] = bitselect(s[23] ^ tmp1, s[23], s[24]); s[24] = bitselect(s[24] ^ tmp2, s[24], tmp1);
-		s[0] ^= keccak_round_constants35[i];
-	}
-	uint2 t[5];
-	t[0] = s[0] ^ s[5] ^ s[10] ^ s[15] ^ s[20];
-	t[1] = s[1] ^ s[6] ^ s[11] ^ s[16] ^ s[21];
-	t[2] = s[2] ^ s[7] ^ s[12] ^ s[17] ^ s[22];
-	t[3] = s[3] ^ s[8] ^ s[13] ^ s[18] ^ s[23];
-	t[4] = s[4] ^ s[9] ^ s[14] ^ s[19] ^ s[24];
+		s[0].x ^= 1;
 
-	s[0] ^= t[4] ^ ROL2(t[1], 1);
-	s[18] ^= t[2] ^ ROL2(t[4], 1);
-	s[24] ^= t[3] ^ ROL2(t[0], 1);
+#pragma unroll 2
+		for (int i = 1; i < 23; i++) 
+		{
 
-	s[3] = ROL2(s[18], 21) ^ ((~ROL2(s[24], 14)) & s[0]);
-}
-#else
+#pragma unroll
+			for (uint32_t x = 0; x < 5; x++)
+				tmpxor[x] = s[x] ^ s[x + 5] ^ s[x + 10] ^ s[x + 15] ^ s[x + 20];
 
-__device__ __forceinline__
-static void keccak_blockv30_80(uint64_t *s, const uint64_t *keccak_round_constants)
-{
-	int i;
-	uint64_t t[5], u[5], v, w;
+			bc[0] = tmpxor[0] ^ ROL2(tmpxor[2], 1);
+			bc[1] = tmpxor[1] ^ ROL2(tmpxor[3], 1);
+			bc[2] = tmpxor[2] ^ ROL2(tmpxor[4], 1);
+			bc[3] = tmpxor[3] ^ ROL2(tmpxor[0], 1);
+			bc[4] = tmpxor[4] ^ ROL2(tmpxor[1], 1);
 
-	/* absorb input */
-	/* theta: c = a[0,i] ^ a[1,i] ^ .. a[4,i] */
-	t[0] = s[0] ^ s[5] ^ s[10];
-	t[1] = s[1] ^ s[6] ^ s[16];
-	t[2] = s[2] ^ s[7];
-	t[3] = s[3] ^ s[8] ;
-	t[4] = s[4] ^ s[9];
+			tmp1 = s[1] ^ bc[0];
 
-	/* theta: d[i] = c[i+4] ^ rotl(c[i+1],1) */
-	u[0] = t[4] ^ ROTL64(t[1], 1);
-	u[1] = t[0] ^ ROTL64(t[2], 1);
-	u[2] = t[1] ^ ROTL64(t[3], 1);
-	u[3] = t[2] ^ ROTL64(t[4], 1);
-	u[4] = t[3] ^ ROTL64(t[0], 1);
+			s[0] ^= bc[4];
+			s[1] = ROL2(s[6] ^ bc[0], 44);
+			s[6] = ROL2(s[9] ^ bc[3], 20);
+			s[9] = ROL2(s[22] ^ bc[1], 61);
+			s[22] = ROL2(s[14] ^ bc[3], 39);
+			s[14] = ROL2(s[20] ^ bc[4], 18);
+			s[20] = ROL2(s[2] ^ bc[1], 62);
+			s[2] = ROL2(s[12] ^ bc[1], 43);
+			s[12] = ROL2(s[13] ^ bc[2], 25);
+			s[13] = ROL2(s[19] ^ bc[3], 8);
+			s[19] = ROL2(s[23] ^ bc[2], 56);
+			s[23] = ROL2(s[15] ^ bc[4], 41);
+			s[15] = ROL2(s[4] ^ bc[3], 27);
+			s[4] = ROL2(s[24] ^ bc[3], 14);
+			s[24] = ROL2(s[21] ^ bc[0], 2);
+			s[21] = ROL2(s[8] ^ bc[2], 55);
+			s[8] = ROL2(s[16] ^ bc[0], 45);
+			s[16] = ROL2(s[5] ^ bc[4], 36);
+			s[5] = ROL2(s[3] ^ bc[2], 28);
+			s[3] = ROL2(s[18] ^ bc[2], 21);
+			s[18] = ROL2(s[17] ^ bc[1], 15);
+			s[17] = ROL2(s[11] ^ bc[0], 10);
+			s[11] = ROL2(s[7] ^ bc[1], 6);
+			s[7] = ROL2(s[10] ^ bc[4], 3);
+			s[10] = ROL2(tmp1, 1);
 
-	/* theta: a[0,i], a[1,i], .. a[4,i] ^= d[i] */
-	s[0] ^= u[0]; s[5] ^= u[0]; s[10] ^= u[0];
-	s[1] ^= u[1]; s[6] ^= u[1]; s[16] ^= u[1];
-	s[2] ^= u[2]; s[7] ^= u[2];
-	s[3] ^= u[3]; s[8] ^= u[3];
-	s[4] ^= u[4]; s[9] ^= u[4];
-
-	/* rho pi: b[..] = rotl(a[..], ..) */
-	v = s[1];
-	s[1] = ROTL64(s[6], 44);
-	s[6] = ROTL64(s[9], 20);
-	s[9] = ROTL64(u[2], 61);
-	s[22] = ROTL64(u[4], 39);
-	s[14] = ROTL64(u[0], 18);
-	s[20] = ROTL64(s[2], 62);
-	s[2] = ROTL64(u[2], 43);
-	s[12] = ROTL64(u[3], 25);
-	s[13] = ROTL64(u[4], 8);
-	s[19] = ROTL64(u[3], 56);
-	s[23] = ROTL64(u[0], 41);
-	s[15] = ROTL64(s[4], 27);
-	s[4] = ROTL64(u[4], 14);
-	s[24] = ROTL64(u[1], 2);
-	s[21] = ROTL64(s[8], 55);
-	s[8] = ROTL64(s[16], 45);
-	s[16] = ROTL64(s[5], 36);
-	s[5] = ROTL64(s[3], 28);
-	s[3] = ROTL64(u[3], 21);
-	s[18] = ROTL64(u[2], 15);
-	s[17] = ROTL64(u[1], 10);
-	s[11] = ROTL64(s[7], 6);
-	s[7] = ROTL64(s[10], 3);
-	s[10] = ROTL64(v, 1);
-
-	/* chi: a[i,j] ^= ~b[i,j+1] & b[i,j+2] */
-	v = s[0]; w = s[1]; s[0] ^= (~w) & s[2]; s[1] ^= (~s[2]) & s[3]; s[2] ^= (~s[3]) & s[4]; s[3] ^= (~s[4]) & v; s[4] ^= (~v) & w;
-	v = s[5]; w = s[6]; s[5] ^= (~w) & s[7]; s[6] ^= (~s[7]) & s[8]; s[7] ^= (~s[8]) & s[9]; s[8] ^= (~s[9]) & v; s[9] ^= (~v) & w;
-	v = s[10]; w = s[11]; s[10] ^= (~w) & s[12]; s[11] ^= (~s[12]) & s[13]; s[12] ^= (~s[13]) & s[14]; s[13] ^= (~s[14]) & v; s[14] ^= (~v) & w;
-	v = s[15]; w = s[16]; s[15] ^= (~w) & s[17]; s[16] ^= (~s[17]) & s[18]; s[17] ^= (~s[18]) & s[19]; s[18] ^= (~s[19]) & v; s[19] ^= (~v) & w;
-	v = s[20]; w = s[21]; s[20] ^= (~w) & s[22]; s[21] ^= (~s[22]) & s[23]; s[22] ^= (~s[23]) & s[24]; s[23] ^= (~s[24]) & v; s[24] ^= (~v) & w;
-
-	/* iota: a[0,0] ^= round constant */
-	s[0] ^= keccak_round_constants[0];
-
-	for (i = 1; i < 23; i++) {
-		/* theta: c = a[0,i] ^ a[1,i] ^ .. a[4,i] */
+			tmp1 = s[0]; tmp2 = s[1]; s[0] = bitselect(s[0] ^ s[2], s[0], s[1]); s[1] = bitselect(s[1] ^ s[3], s[1], s[2]); s[2] = bitselect(s[2] ^ s[4], s[2], s[3]); s[3] = bitselect(s[3] ^ tmp1, s[3], s[4]); s[4] = bitselect(s[4] ^ tmp2, s[4], tmp1);
+			tmp1 = s[5]; tmp2 = s[6]; s[5] = bitselect(s[5] ^ s[7], s[5], s[6]); s[6] = bitselect(s[6] ^ s[8], s[6], s[7]); s[7] = bitselect(s[7] ^ s[9], s[7], s[8]); s[8] = bitselect(s[8] ^ tmp1, s[8], s[9]); s[9] = bitselect(s[9] ^ tmp2, s[9], tmp1);
+			tmp1 = s[10]; tmp2 = s[11]; s[10] = bitselect(s[10] ^ s[12], s[10], s[11]); s[11] = bitselect(s[11] ^ s[13], s[11], s[12]); s[12] = bitselect(s[12] ^ s[14], s[12], s[13]); s[13] = bitselect(s[13] ^ tmp1, s[13], s[14]); s[14] = bitselect(s[14] ^ tmp2, s[14], tmp1);
+			tmp1 = s[15]; tmp2 = s[16]; s[15] = bitselect(s[15] ^ s[17], s[15], s[16]); s[16] = bitselect(s[16] ^ s[18], s[16], s[17]); s[17] = bitselect(s[17] ^ s[19], s[17], s[18]); s[18] = bitselect(s[18] ^ tmp1, s[18], s[19]); s[19] = bitselect(s[19] ^ tmp2, s[19], tmp1);
+			tmp1 = s[20]; tmp2 = s[21]; s[20] = bitselect(s[20] ^ s[22], s[20], s[21]); s[21] = bitselect(s[21] ^ s[23], s[21], s[22]); s[22] = bitselect(s[22] ^ s[24], s[22], s[23]); s[23] = bitselect(s[23] ^ tmp1, s[23], s[24]); s[24] = bitselect(s[24] ^ tmp2, s[24], tmp1);
+			s[0] ^= keccak_round_constants35[i];
+		}
+		uint2 t[5];
 		t[0] = s[0] ^ s[5] ^ s[10] ^ s[15] ^ s[20];
 		t[1] = s[1] ^ s[6] ^ s[11] ^ s[16] ^ s[21];
 		t[2] = s[2] ^ s[7] ^ s[12] ^ s[17] ^ s[22];
 		t[3] = s[3] ^ s[8] ^ s[13] ^ s[18] ^ s[23];
 		t[4] = s[4] ^ s[9] ^ s[14] ^ s[19] ^ s[24];
 
-		/* theta: d[i] = c[i+4] ^ rotl(c[i+1],1) */
-		u[0] = t[4] ^ ROTL64(t[1], 1);
-		u[1] = t[0] ^ ROTL64(t[2], 1);
-		u[2] = t[1] ^ ROTL64(t[3], 1);
-		u[3] = t[2] ^ ROTL64(t[4], 1);
-		u[4] = t[3] ^ ROTL64(t[0], 1);
+		s[0] ^= t[4] ^ ROL2(t[1], 1);
+		s[18] ^= t[2] ^ ROL2(t[4], 1);
+		s[24] ^= t[3] ^ ROL2(t[0], 1);
 
-		/* theta: a[0,i], a[1,i], .. a[4,i] ^= d[i] */
-		s[0] ^= u[0]; s[5] ^= u[0]; s[10] ^= u[0]; s[15] ^= u[0]; s[20] ^= u[0];
-		s[1] ^= u[1]; s[6] ^= u[1]; s[11] ^= u[1]; s[16] ^= u[1]; s[21] ^= u[1];
-		s[2] ^= u[2]; s[7] ^= u[2]; s[12] ^= u[2]; s[17] ^= u[2]; s[22] ^= u[2];
-		s[3] ^= u[3]; s[8] ^= u[3]; s[13] ^= u[3]; s[18] ^= u[3]; s[23] ^= u[3];
-		s[4] ^= u[4]; s[9] ^= u[4]; s[14] ^= u[4]; s[19] ^= u[4]; s[24] ^= u[4];
+		s[3] = ROL2(s[18], 21) ^ ((~ROL2(s[24], 14)) & s[0]);
 
-		/* rho pi: b[..] = rotl(a[..], ..) */
-		v = s[ 1];
-		s[ 1] = ROTL64(s[ 6], 44);
-		s[ 6] = ROTL64(s[ 9], 20);
-		s[ 9] = ROTL64(s[22], 61);
-		s[22] = ROTL64(s[14], 39);
-		s[14] = ROTL64(s[20], 18);
-		s[20] = ROTL64(s[ 2], 62);
-		s[ 2] = ROTL64(s[12], 43);
-		s[12] = ROTL64(s[13], 25);
-		s[13] = ROTL64(s[19],  8);
-		s[19] = ROTL64(s[23], 56);
-		s[23] = ROTL64(s[15], 41);
-		s[15] = ROTL64(s[ 4], 27);
-		s[ 4] = ROTL64(s[24], 14);
-		s[24] = ROTL64(s[21],  2);
-		s[21] = ROTL64(s[ 8], 55);
-		s[ 8] = ROTL64(s[16], 45);
-		s[16] = ROTL64(s[ 5], 36);
-		s[ 5] = ROTL64(s[ 3], 28);
-		s[ 3] = ROTL64(s[18], 21);
-		s[18] = ROTL64(s[17], 15);
-		s[17] = ROTL64(s[11], 10);
-		s[11] = ROTL64(s[ 7],  6);
-		s[ 7] = ROTL64(s[10],  3);
-		s[10] = ROTL64(    v,  1);
 
-		/* chi: a[i,j] ^= ~b[i,j+1] & b[i,j+2] */
-		v = s[ 0]; w = s[ 1]; s[ 0] ^= (~w) & s[ 2]; s[ 1] ^= (~s[ 2]) & s[ 3]; s[ 2] ^= (~s[ 3]) & s[ 4]; s[ 3] ^= (~s[ 4]) & v; s[ 4] ^= (~v) & w;
-		v = s[ 5]; w = s[ 6]; s[ 5] ^= (~w) & s[ 7]; s[ 6] ^= (~s[ 7]) & s[ 8]; s[ 7] ^= (~s[ 8]) & s[ 9]; s[ 8] ^= (~s[ 9]) & v; s[ 9] ^= (~v) & w;
-		v = s[10]; w = s[11]; s[10] ^= (~w) & s[12]; s[11] ^= (~s[12]) & s[13]; s[12] ^= (~s[13]) & s[14]; s[13] ^= (~s[14]) & v; s[14] ^= (~v) & w;
-		v = s[15]; w = s[16]; s[15] ^= (~w) & s[17]; s[16] ^= (~s[17]) & s[18]; s[17] ^= (~s[18]) & s[19]; s[18] ^= (~s[19]) & v; s[19] ^= (~v) & w;
-		v = s[20]; w = s[21]; s[20] ^= (~w) & s[22]; s[21] ^= (~s[22]) & s[23]; s[22] ^= (~s[23]) & s[24]; s[23] ^= (~s[24]) & v; s[24] ^= (~v) & w;
-
-		/* iota: a[0,0] ^= round constant */
-		s[0] ^= keccak_round_constants[i];
-	}
-	t[0] = s[0] ^ s[5] ^ s[10] ^ s[15] ^ s[20];
-	t[1] = s[1] ^ s[6] ^ s[11] ^ s[16] ^ s[21];
-	t[2] = s[2] ^ s[7] ^ s[12] ^ s[17] ^ s[22];
-	t[3] = s[3] ^ s[8] ^ s[13] ^ s[18] ^ s[23];
-	t[4] = s[4] ^ s[9] ^ s[14] ^ s[19] ^ s[24];
-
-	s[0] ^= t[4] ^ ROTL64(t[1], 1);
-	s[18] ^= t[2] ^ ROTL64(t[4], 1);
-	s[24] ^= t[3] ^ ROTL64(t[0], 1);
-
-	s[3] = ROTL64(s[18], 21) ^ ((~ROTL64(s[24], 14)) & s[0]);
-}
-#endif
-
-__global__ __launch_bounds__(128,4)
-void keccak256_gpu_hash_80(uint32_t threads, uint32_t startNounce, void *outputHash, uint32_t *resNounce)
-{
-	uint32_t thread = (blockDim.x * blockIdx.x + threadIdx.x);
-	if (thread < threads)
-	{
-		uint32_t nounce = startNounce + thread;
-
-#if __CUDA_ARCH__ >= 350
-		uint2 keccak_gpu_state[25];
-		#pragma unroll 25
-		for (int i=0; i<25; i++) {
-			if (i<9) keccak_gpu_state[i] = vectorize(c_PaddedMessage80[i]);
-			else     keccak_gpu_state[i] = UINT2(0, 0);
-		}
-
-		keccak_gpu_state[9]= vectorize(c_PaddedMessage80[9]);
-		keccak_gpu_state[9].y = cuda_swab32(nounce);
-		keccak_gpu_state[10] = UINT2(1, 0);
-		keccak_gpu_state[16] = UINT2(0, 0x80000000);
-
-		keccak_blockv35_80(keccak_gpu_state);
-		if (devectorize(keccak_gpu_state[3]) <= ((uint64_t*)pTarget)[3])
-		{
-			uint32_t tmp = atomicCAS(resNounce, 0xffffffff, nounce);
-			if (tmp != 0xffffffff)
-				resNounce[1] = nounce;
-	}
-#else
-		uint64_t keccak_gpu_state[25];
-		#pragma unroll 25
-		for (int i=0; i<25; i++) {
-			if (i<9) keccak_gpu_state[i] = c_PaddedMessage80[i];
-			else     keccak_gpu_state[i] = 0;
-		}
-		keccak_gpu_state[9]  = REPLACE_HIWORD(c_PaddedMessage80[9], cuda_swab32(nounce));
-		keccak_gpu_state[10] = 0x0000000000000001;
-		keccak_gpu_state[16] = 0x8000000000000000;
-
-		keccak_blockv30_80(keccak_gpu_state, keccak_round_constants);
-		if (keccak_gpu_state[3] <= ((uint64_t*)pTarget)[3])
+		if (devectorize(s[3]) <= ((uint64_t*)pTarget)[3])
 		{
 			uint32_t tmp = atomicCAS(resNounce, 0xffffffff, nounce);
 			if (tmp != 0xffffffff)
 				resNounce[1] = nounce;
 		}
-#endif
 	}
 }
 
