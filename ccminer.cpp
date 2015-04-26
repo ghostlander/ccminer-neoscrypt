6cb3b05a2672d4a6d58a6a630977ae5807a27d7f
sp-hash <rune.stensland@bradyplc.com>
2015-04-26 15:15:12 +0200

    fixed default diff for keccak/skein

--- ccminer.cpp
+++ ccminer.cpp
@@ -1305,17 +1305,17 @@ static void *miner_thread(void *userdata)
 			switch (opt_algo) {
 			case ALGO_BLAKECOIN:
 			case ALGO_BLAKE:
+			case ALGO_SKEIN:
+			case ALGO_KECCAK:
 				minmax = 0x80000000U;
 				break;
 			case ALGO_BITCOIN:
-			case ALGO_KECCAK:
 			case ALGO_WHCX:
 				minmax = 0x40000000U;
 				break;
 			case ALGO_DOOM:
 			case ALGO_JACKPOT:
 			case ALGO_LUFFA_DOOM:
-			case ALGO_SKEIN:
 				minmax = 0x2000000;
 				break;
 			case ALGO_QUARK:
