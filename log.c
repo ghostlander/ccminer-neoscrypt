0f7153b3af00c75e09c225954313b854d656e7d7
sp-hash <rune.stensland@bradyplc.com>
2015-08-30 22:00:38 +0200

    revert Nelsons output. (doesn't work on windows)

--- log.c
+++ log.c
@@ -16,116 +16,79 @@ static pthread_mutex_t  applog_lock = PTHREAD_MUTEX_INITIALIZER;
 void applog(int prio, const char *fmt, ...)
 {
 	va_list ap;
-
 	va_start(ap, fmt);
-
-#ifdef HAVE_SYSLOG_H
-	if (use_syslog) {
-		va_list ap2;
-		char *buf;
-		int len;
-
-		/* custom colors to syslog prio */
-		if (prio > LOG_DEBUG) {
-			switch (prio) {
-				case LOG_BLUE: prio = LOG_NOTICE; break;
-			}
-		}
-
-		va_copy(ap2, ap);
-		len = vsnprintf(NULL, 0, fmt, ap2) + 1;
-		va_end(ap2);
-		buf = (char*) alloca(len);
-		if (vsnprintf(buf, len, fmt, ap) >= 0)
-			syslog(prio, "%s", buf);
-	}
-#else
-	if (0) {}
-#endif
-	else {
-		const char* color = "";
-		char* hdr     = NULL;
-		int   hdr_len = 0;
-		struct tm tm;
-		time_t now = time(NULL);
-		int    tid = gettid();
-
-		localtime_r(&now, &tm);
+	#ifdef HAVE_SYSLOG_H
+			if (use_syslog) {
+				va_list ap2;
+				char *buf;
+				int len;
 		
-		if (use_colors) {
-			switch (prio) {
-				case LOG_ERR:     color = CL_RED; break;
-				case LOG_WARNING: color = CL_YLW; break;
-				case LOG_NOTICE:  color = CL_WHT; break;
-				case LOG_INFO:    color = ""; break;
-				case LOG_DEBUG:   color = CL_GRY; break;
-
-				case LOG_BLUE:
-					prio = LOG_NOTICE;
-					color = CL_CYN;
-					break;
+					/* custom colors to syslog prio */
+					if (prio > LOG_DEBUG) {
+						switch (prio) {
+								case LOG_BLUE: prio = LOG_NOTICE; break;
+							}
+					}
+		
+					va_copy(ap2, ap);
+				len = vsnprintf(NULL, 0, fmt, ap2) + 1;
+				va_end(ap2);
+				buf = (char*) alloca(len);
+				if (vsnprintf(buf, len, fmt, ap) >= 0)
+						syslog(prio, "%s", buf);
 			}
-		} else if (prio == LOG_BLUE) {
-			prio = LOG_NOTICE;
+	#else
+		 if (0) {}
+	#endif
+		 else {
+		const char* color = "";
+		char *f;
+		int len;
+		struct tm tm, *tm_p;
+		time_t now = time(NULL);
+		
+		pthread_mutex_lock(&applog_lock);
+		tm_p = localtime(&now);
+		memcpy(&tm, tm_p, sizeof(tm));
+		pthread_mutex_unlock(&applog_lock);
+		
+			switch (prio) {
+			case LOG_ERR:     color = CL_RED; break;
+			case LOG_WARNING: color = CL_YLW; break;
+			case LOG_NOTICE:  color = CL_WHT; break;
+			case LOG_INFO:    color = ""; break;
+			case LOG_DEBUG:   color = CL_GRY; break;
+			
+			case LOG_BLUE:
+				prio = LOG_NOTICE;
+				color = CL_CYN;
+			break;
+					
 		}
-#define HDR_FMT "[%d-%02d-%02d %02d:%02d:%02d] tid(%#010x)%s "
-
-		hdr_len = snprintf(hdr, hdr_len, HDR_FMT,
+		if (!use_colors)
+			 color = "";
+		
+		len = 40 + (int)strlen(fmt) + 2;
+		f = (char*)alloca(len);
+		sprintf(f, "[%d-%02d-%02d %02d:%02d:%02d]%s %s%s\n",
 			tm.tm_year + 1900,
 			tm.tm_mon + 1,
 			tm.tm_mday,
 			tm.tm_hour,
 			tm.tm_min,
 			tm.tm_sec,
-			tid,
-			color);
-
-		if (hdr_len > 0) {
-			hdr_len += 1; /* Make room for NUL terminal */
-			if (NULL != (hdr = alloca(hdr_len * sizeof(char)))) {
-				char* msg     = NULL;
-				int   msg_len = 0;
-				va_list ap2;
-
-				va_copy(ap2, ap);
-				msg_len = vsnprintf(msg, msg_len, fmt, ap2);
-				va_end(ap2);
-
-				if (msg_len > 0) {
-					msg_len += 1; /* Make room for NUL terminal */
-
-					if (NULL != (msg = alloca(msg_len * sizeof(char)))) {
-						const char* eol = "\n";
-
-						if (use_colors)
-							eol = CL_N "\n";
-
-						snprintf(hdr, hdr_len, HDR_FMT,
-							tm.tm_year + 1900,
-							tm.tm_mon + 1,
-							tm.tm_mday,
-							tm.tm_hour,
-							tm.tm_min,
-							tm.tm_sec,
-							tid,
-							color);
-						vsnprintf(msg, msg_len, fmt, ap);
-
-						pthread_mutex_lock(&applog_lock);
-						fwrite(hdr, sizeof(char), hdr_len, stderr);
-						fwrite(msg, sizeof(char), msg_len, stderr);
-						fwrite(eol, sizeof(char), strlen(eol), stderr);
-						pthread_mutex_unlock(&applog_lock);
-
-						fflush(stderr);
-					}
-				}
-			}
-		}
+			color,
+			fmt,
+			use_colors ? CL_N : ""
+			 );
+		pthread_mutex_lock(&applog_lock);
+		vfprintf(stderr, f, ap);	/* atomic write to stderr */
+		fflush(stderr);
+		pthread_mutex_unlock(&applog_lock);
+		
 	}
 	va_end(ap);
 }
-
 /* to debug diff in data */
 extern void applog_compare_hash(uchar *hash, uchar *hash2)
 {
