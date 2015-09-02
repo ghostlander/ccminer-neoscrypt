4b9cb12c40f1e4e7bd35f82df2e424eadd1e6f45
sp-hash <rune.stensland@bradyplc.com>
2015-09-02 16:13:01 +0200

    Blakecoin 70% faster

--- Algo256/blake256.cu
+++ Algo256/blake256.cu
@@ -101,7 +101,6 @@ static const uint32_t __align__(32) c_IV256[8] = {
 	}
 
 /* Second part (64-80) msg never change, store it */
-
 __device__ static
 void blake256_compress(uint32_t *h, const uint32_t *block, const uint32_t T0, const int rounds)
 {
@@ -303,6 +302,155 @@ void blake256_compress(uint32_t *h, const uint32_t *block, const uint32_t T0, co
 #endif
 }
 
+
+/* Second part (64-80) msg never change, store it */
+__device__ static
+void blake256_compress_8(uint32_t *h, const uint32_t *block, const uint32_t T0, const int rounds)
+{
+	uint32_t /*_ALIGN(8)*/ m[16];
+	uint32_t v[16];
+
+	m[0] = block[0];
+	m[1] = block[1];
+	m[2] = block[2];
+	m[3] = block[3];
+
+	const uint32_t c_u256[16] = 
+	{
+		SPH_C32(0x243F6A88), SPH_C32(0x85A308D3),
+		SPH_C32(0x13198A2E), SPH_C32(0x03707344),
+		SPH_C32(0xA4093822), SPH_C32(0x299F31D0),
+		SPH_C32(0x082EFA98), SPH_C32(0xEC4E6C89),
+		SPH_C32(0x452821E6), SPH_C32(0x38D01377),
+		SPH_C32(0xBE5466CF), SPH_C32(0x34E90C6C),
+		SPH_C32(0xC0AC29B7), SPH_C32(0xC97C50DD),
+		SPH_C32(0x3F84D5B5), SPH_C32(0xB5470917)
+	};
+
+	const uint32_t c_Padding[16] = {
+		0, 0, 0, 0,
+		0x80000000UL, 0, 0, 0,
+		0, 0, 0, 0,
+		0, 1, 0, 640,
+	};
+
+
+#pragma unroll
+	for (uint32_t i = 4; i < 16; i++) 
+	{
+#if PRECALC64
+		m[i] = c_Padding[i];
+#else
+		m[i] = (T0 == 0x200) ? block[i] : c_Padding[i];
+#endif
+	}
+
+	//#pragma unroll 8
+	for(uint32_t i = 0; i < 8; i++)
+		v[i] = h[i];
+
+	v[ 8] = c_u256[0];
+	v[ 9] = c_u256[1];
+	v[10] = c_u256[2];
+	v[11] = c_u256[3];
+
+	v[12] = c_u256[4] ^ T0;
+	v[13] = c_u256[5] ^ T0;
+	v[14] = c_u256[6];
+	v[15] = c_u256[7];
+
+	//	{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },
+	GSPREC(0, 4, 0x8, 0xC,0,1);
+	GSPREC(1, 5, 0x9, 0xD,2,3);
+	GSPREC(2, 6, 0xA, 0xE, 4,5);
+	GSPREC(3, 7, 0xB, 0xF, 6,7);
+	GSPREC(0, 5, 0xA, 0xF, 8,9);
+	GSPREC(1, 6, 0xB, 0xC, 10,11);
+	GSPREC(2, 7, 0x8, 0xD, 12,13);
+	GSPREC(3, 4, 0x9, 0xE, 14,15);
+	//	{ 14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3 },
+	GSPREC(0, 4, 0x8, 0xC, 14, 10);
+	GSPREC(1, 5, 0x9, 0xD, 4, 8);
+	GSPREC(2, 6, 0xA, 0xE, 9, 15);
+	GSPREC(3, 7, 0xB, 0xF, 13, 6);
+	GSPREC(0, 5, 0xA, 0xF, 1, 12);
+	GSPREC(1, 6, 0xB, 0xC, 0, 2);
+	GSPREC(2, 7, 0x8, 0xD, 11, 7);
+	GSPREC(3, 4, 0x9, 0xE, 5, 3);
+	//	{ 11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4 },
+	GSPREC(0, 4, 0x8, 0xC, 11, 8);
+	GSPREC(1, 5, 0x9, 0xD, 12, 0);
+	GSPREC(2, 6, 0xA, 0xE, 5, 2);
+	GSPREC(3, 7, 0xB, 0xF, 15, 13);
+	GSPREC(0, 5, 0xA, 0xF, 10, 14);
+	GSPREC(1, 6, 0xB, 0xC, 3, 6);
+	GSPREC(2, 7, 0x8, 0xD, 7, 1);
+	GSPREC(3, 4, 0x9, 0xE, 9, 4);
+	//	{ 7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8 },
+	GSPREC(0, 4, 0x8, 0xC, 7, 9);
+	GSPREC(1, 5, 0x9, 0xD, 3, 1);
+	GSPREC(2, 6, 0xA, 0xE, 13, 12);
+	GSPREC(3, 7, 0xB, 0xF, 11, 14);
+	GSPREC(0, 5, 0xA, 0xF, 2, 6);
+	GSPREC(1, 6, 0xB, 0xC, 5, 10);
+	GSPREC(2, 7, 0x8, 0xD, 4, 0);
+	GSPREC(3, 4, 0x9, 0xE, 15, 8);
+
+	//	{ 9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13 },
+	GSPREC(0, 4, 0x8, 0xC, 9, 0);
+	GSPREC(1, 5, 0x9, 0xD, 5, 7);
+	GSPREC(2, 6, 0xA, 0xE, 2, 4);
+	GSPREC(3, 7, 0xB, 0xF, 10, 15);
+	GSPREC(0, 5, 0xA, 0xF, 14, 1);
+	GSPREC(1, 6, 0xB, 0xC, 11, 12);
+	GSPREC(2, 7, 0x8, 0xD, 6, 8);
+	GSPREC(3, 4, 0x9, 0xE, 3, 13);
+	//	{ 2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9 },
+	GSPREC(0, 4, 0x8, 0xC, 2, 12);
+	GSPREC(1, 5, 0x9, 0xD, 6, 10);
+	GSPREC(2, 6, 0xA, 0xE, 0, 11);
+	GSPREC(3, 7, 0xB, 0xF, 8, 3);
+	GSPREC(0, 5, 0xA, 0xF, 4, 13);
+	GSPREC(1, 6, 0xB, 0xC, 7, 5);
+	GSPREC(2, 7, 0x8, 0xD, 15, 14);
+	GSPREC(3, 4, 0x9, 0xE, 1, 9);
+
+	//	{ 12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11 },
+	GSPREC(0, 4, 0x8, 0xC, 12, 5);
+	GSPREC(1, 5, 0x9, 0xD, 1, 15);
+	GSPREC(2, 6, 0xA, 0xE, 14, 13);
+	GSPREC(3, 7, 0xB, 0xF, 4, 10);
+	GSPREC(0, 5, 0xA, 0xF, 0, 7);
+	GSPREC(1, 6, 0xB, 0xC, 6, 3);
+	GSPREC(2, 7, 0x8, 0xD, 9, 2);
+	GSPREC(3, 4, 0x9, 0xE, 8, 11);
+
+	//	{ 13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10 },
+	GSPREC(0, 4, 0x8, 0xC, 13, 11);
+	GSPREC(1, 5, 0x9, 0xD, 7, 14);
+	GSPREC(2, 6, 0xA, 0xE, 12, 1);
+	GSPREC(3, 7, 0xB, 0xF, 3, 9);
+	GSPREC(0, 5, 0xA, 0xF, 5, 0);
+	GSPREC(1, 6, 0xB, 0xC, 15, 4);
+	GSPREC(2, 7, 0x8, 0xD, 8, 6);
+	GSPREC(3, 4, 0x9, 0xE, 2, 10);
+	//	{ 6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5 },
+
+
+#if PRECALC64
+	// only compute h6 & 7
+	h[6U] ^= v[6U] ^ v[14U];
+	h[7U] ^= v[7U] ^ v[15U];
+#else
+	//#pragma unroll 16
+	for (uint32_t i = 0; i < 16; i++) {
+		uint32_t j = i & 7U;
+		h[j] ^= v[i];
+	}
+#endif
+}
+
+
 #if !PRECALC64 /* original method */
 __global__
 void blake256_gpu_hash_80(const uint32_t threads, const uint32_t startNonce, uint32_t *resNonce,
@@ -448,6 +596,57 @@ void blake256_gpu_hash_16(const uint32_t threads, const uint32_t startNonce, uin
 	}
 }
 
+
+__global__
+void blake256_gpu_hash_16_8(const uint32_t threads, const uint32_t startNonce, uint32_t *resNonce,
+const uint64_t highTarget, const int rounds, const bool trace)
+{
+	uint32_t thread = (blockDim.x * blockIdx.x + threadIdx.x);
+	if (thread < threads)
+	{
+		const uint32_t nonce = startNonce + thread;
+		uint32_t _ALIGN(16) h[8];
+
+#pragma unroll
+		for (int i = 0; i < 8; i++) {
+			h[i] = d_data[i];
+		}
+
+		// ------ Close: Bytes 64 to 80 ------
+
+		uint32_t _ALIGN(16) ending[4];
+		ending[0] = d_data[8];
+		ending[1] = d_data[9];
+		ending[2] = d_data[10];
+		ending[3] = nonce; /* our tested value */
+
+		blake256_compress_8(h, ending, 640, rounds);
+
+		if (h[7] == 0 && cuda_swab32(h[6]) <= highTarget) {
+#if NBN == 2
+			/* keep the smallest nonce, + extra one if found */
+			if (resNonce[0] > nonce) {
+				resNonce[1] = resNonce[0];
+				resNonce[0] = nonce;
+			}
+			else
+				resNonce[1] = nonce;
+#else
+			resNonce[0] = nonce;
+#endif
+#ifdef _DEBUG
+			if (trace) {
+				uint64_t high64 = ((uint64_t*)h)[3];
+				printf("gpu:  %16llx\n", high64);
+				printf("gpu: %08x.%08x\n", h[7], h[6]);
+				printf("tgt:  %16llx\n", highTarget);
+			}
+#endif
+		}
+	}
+}
+
+
 __host__
 static uint32_t blake256_cpu_hash_16(const int thr_id, const uint32_t threads, const uint32_t startNonce, const uint64_t highTarget,
 	const int8_t rounds)
@@ -460,9 +659,16 @@ static uint32_t blake256_cpu_hash_16(const int thr_id, const uint32_t threads, c
 	/* Check error on Ctrl+C or kill to prevent segfaults on exit */
 	if (cudaMemset(d_resNonce[thr_id], 0xff, NBN*sizeof(uint32_t)) != cudaSuccess)
 		return result;
-
-	blake256_gpu_hash_16 <<<grid, block>>> (threads, startNonce, d_resNonce[thr_id], highTarget, (int) rounds, opt_tracegpu);
-//	cudaDeviceSynchronize();
+	if (rounds == 8)
+	{
+		blake256_gpu_hash_16_8 << <grid, block >> > (threads, startNonce, d_resNonce[thr_id], highTarget, (int)rounds, opt_tracegpu);
+	}
+	else
+	{
+		blake256_gpu_hash_16 << <grid, block >> > (threads, startNonce, d_resNonce[thr_id], highTarget, (int)rounds, opt_tracegpu);
+	}
+	
+	//	cudaDeviceSynchronize();
 	if (cudaSuccess == cudaMemcpy(h_resNonce[thr_id], d_resNonce[thr_id], NBN*sizeof(uint32_t), cudaMemcpyDeviceToHost)) {
 		//cudaDeviceSynchronize(); /* seems no more required */
 		result = h_resNonce[thr_id][0];
