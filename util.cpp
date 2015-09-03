75cf466f01c8c5fbb00c841a95e46050a1beca74
sp-hash <rune.stensland@bradyplc.com>
2015-09-03 17:28:06 +0200

    fix bug

--- util.cpp
+++ util.cpp
@@ -1210,7 +1210,6 @@ static bool stratum_set_difficulty(struct stratum_ctx *sctx, json_t *params)
 	/* store for api stats */
 	if (diff != global_diff) 
 	{
-		scan_abort_flag = true;
 		global_diff = diff;
 		applog(LOG_WARNING, "Stratum difficulty set to %g", diff);
 	}
