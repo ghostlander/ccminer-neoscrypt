b9081d3d20c51de11f6b5de6a6b10309f1b479dc
pallas1 <pallas1@users.noreply.github.com>
2015-09-16 16:24:17 +0200

    Startup cosmetics

--- ccminer.cpp
+++ ccminer.cpp
@@ -2680,9 +2680,9 @@ int main(int argc, char *argv[])
 #else
 	printf("\tBuilt with the nVidia CUDA SDK 6.5\n\n");
 #endif
-	printf("  Based on pooler cpuminer 2.3.2 and the tpruvot@github fork\n ");
+	printf("  Based on pooler cpuminer 2.3.2 and the tpruvot@github fork\n");
 	printf("  CUDA support by Christian Buchner, Christian H. and DJM34\n");
-	printf("  Includes optimizations implemented by sp , klaust, tpruvot,tsiv and pallas. \n\n");
+	printf("  Includes optimizations implemented by sp, klaust, tpruvot, tsiv and pallas.\n\n");
 
 	rpc_user = strdup("");
 	rpc_pass = strdup("");
