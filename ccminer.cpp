ghostlander/ccminer-neoscrypt

commit 97a9ef1e34f782932a15866e38fce60daba094a0

Added the bitcredits algorithm from the DJM34 fork

ccminer.cpp
@@ -82,6 +82,7 @@ struct workio_cmd {
 
 enum sha_algos {
 	ALGO_ANIME,
+	ALGO_BITC,
 	ALGO_BITCOIN,
 	ALGO_BLAKE,
 	ALGO_BLAKECOIN,
@@ -121,6 +122,7 @@ enum sha_algos {
 
 static const char *algo_names[] = {
 	"anime",
+	"credit",
 	"bitcoin",
 	"blake",
 	"blakecoin",
@@ -164,7 +166,7 @@ bool want_longpoll = true;
 bool have_longpoll = false;
 bool want_stratum = true;
 bool have_stratum = false;
-bool allow_gbt = true;
+bool allow_gbt = false;
 bool check_dups = false;
 static bool submit_old = false;
 bool use_syslog = false;
@@ -261,6 +263,7 @@ Options:\n\
 			bitcoin     Bitcoin\n\
 			blake       Blake 256 (SFR/NEOS)\n\
 			blakecoin   Fast Blake 256 (8 rounds)\n\
+			 credit      Credit\n\
 			deep        Deepcoin\n\
 			dmd-gr      Diamond-Groestl\n\
 			fresh       Freshcoin (shavite 80)\n\
@@ -522,12 +525,17 @@ static bool jobj_binary(const json_t *obj, const char *key,
 	return true;
 }
 
+
 static bool work_decode(const json_t *val, struct work *work)
 {
-	int data_size;
+	int data_size, midstate_size;
 	switch (opt_algo) {
 	case ALGO_NEO:
 		data_size = 84;
+		break;
+	case ALGO_BITC:
+		data_size = 168;
+		midstate_size = sizeof(work->midstate);
 		break;
 	default:
 		data_size = 128; // original sizeof(work->data); however data is now 64*4bit
@@ -545,15 +553,30 @@ static bool work_decode(const json_t *val, struct work *work)
 		applog(LOG_ERR, "JSON inval target");
 		return false;
 	}
+
+	if (opt_algo == ALGO_BITC) {
+		if (unlikely(!jobj_binary(val, "midstate", work->midstate, midstate_size))) {
+			applog(LOG_ERR, "JSON inval midstate");
+			return false;
+		}
+
+		for (i = 0; i < midstate_size>>2; i++)
+			work->midstate[i] = le32dec(work->midstate + i);
+	}
+
+
 	if (opt_algo == ALGO_HEAVY) {
 		if (unlikely(!jobj_binary(val, "maxvote", &work->maxvote, sizeof(work->maxvote)))) {
 			work->maxvote = 2048;
 		}
 	}
 	else work->maxvote = 0;
-
+	//	printf("the data: \n");
 	for (i = 0; i < adata_sz; i++)
 		work->data[i] = le32dec(work->data + i);
+
+	//		printf("i %d %08x \n",i, work->data[i]);}
+	//	printf("\n");
 	for (i = 0; i < atarget_sz; i++)
 		work->target[i] = le32dec(work->target + i);
 
@@ -573,7 +596,6 @@ static bool work_decode(const json_t *val, struct work *work)
 
 	return true;
 }
-
 
 /**
  * Calculate the work difficulty as double
@@ -1251,7 +1273,7 @@ static void *miner_thread(void *userdata)
 		uint64_t max64, minmax = 0x100000;
 
 		// &work.data[19]
-		int wcmplen = 76;
+		int wcmplen = (opt_algo == ALGO_BITC) ? 140 : 76;
 		uint32_t *nonceptr = (uint32_t*) (((char*)work.data) + wcmplen);
 
 		if (have_stratum) {
@@ -1572,7 +1594,9 @@ static void *miner_thread(void *userdata)
 			case ALGO_YES:
 			rc = scanhash_yescrypt(thr_id, work.data, work.target, max_nonce, &hashes_done);
 			break;
-						
+		case ALGO_BITC:
+			rc = scanhash_bitcredit(thr_id, work.data, work.target, work.midstate, max_nonce, &hashes_done);
+			break;			
 		default:
 			/* should never happen */
 			goto out;
@@ -1624,7 +1648,7 @@ static void *miner_thread(void *userdata)
 		if (check_dups)
 			hashlog_remember_scan_range(&work);
 
-		if ( !opt_quiet && (loopcnt > 0))
+		if (!opt_quiet && (opt_algo == ALGO_BITC) ? (loopcnt % 400 == 0) : (loopcnt))
 		{
 			double hashrate = 0.0;
 

Makefile.am
@@ -45,7 +45,7 @@ ccminer_SOURCES	= elist.h miner.h compat.h \
 			  quark/cuda_quark_compactionTest.cu  \
 			  cuda_nist5.cu pentablake.cu skein.cu\
 			  sph/bmw.c sph/blake.c sph/groestl.c sph/jh.c sph/keccak.c sph/skein.c \
-			  sph/cubehash.c sph/echo.c sph/luffa.c sph/sha2.c sph/shavite.c sph/simd.c \
+			  sph/cubehash.c sph/echo.c sph/luffa.c sph/sha2.c sph/sph_sha2.c sph/shavite.c sph/simd.c \
 			  sph/hamsi.c sph/hamsi_helper.c sph/sph_hamsi.h \
 			  sph/shabal.c sph/whirlpool.c sph/sha2big.c sph/haval.c \
 			  qubit/qubit.cu qubit/qubit_luffa512.cu qubit/deep.cu qubit/doom.cu \
@@ -60,6 +60,7 @@ ccminer_SOURCES	= elist.h miner.h compat.h \
 			  bitcoin.cu cuda_bitcoin.cu\
 			  x15/cuda_whirlpoolx.cu x15/whirlpoolx.cu \
 			  neoscrypt/neoscrypt.cu neoscrypt/cuda_neoscrypt.cu \
+              bitcredit/cuda_bitcredit.cu bitcredit/bitcredit.cu \
   			  yescrypt/cuda_yescrypt.cu yescrypt/yescrypt.cu
 
 # scrypt

bitcredit/bitcredit.cu
@@ -1,0 +1,96 @@
+
+extern "C"
+{
+#include "sph/sph_types.h"
+#include "sph/sph_sha2.h"
+#include "miner.h"
+}
+
+#include "cuda_helper.h"
+
+
+
+static uint32_t *d_hash[MAX_GPUS] ;
+
+
+
+extern void bitcredit_setBlockTarget(uint32_t * data,const uint32_t * midstate, const void *ptarget);
+extern void bitcredit_cpu_init(uint32_t thr_id, int threads, uint32_t* hash);
+extern uint32_t bitcredit_cpu_hash(uint32_t thr_id, int threads, uint32_t startNounce, int order);
+
+ void credithash(void *state, const void *input)
+{
+
+	sph_sha256_context sha1,sha2;
+	uint32_t hash[8],hash2[8];
+
+	sph_sha256_init(&sha1);
+	sph_sha256(&sha1, input, 168);
+	sph_sha256_close(&sha1, hash);
+
+
+	sph_sha256_init(&sha2);
+	sph_sha256(&sha2, hash, 32);
+	sph_sha256_close(&sha2, hash2);
+
+
+	memcpy(state, hash2, 32);
+}
+
+
+     
+extern "C" int scanhash_bitcredit(int thr_id, uint32_t *pdata,
+	const uint32_t *ptarget, const uint32_t *midstate, uint32_t max_nonce,
+	unsigned long *hashes_done)
+{
+	const uint32_t first_nonce = pdata[35];
+	if (opt_benchmark)
+		((uint32_t*)ptarget)[7] = 0x1;
+
+	const uint32_t Htarg = ptarget[7];
+	int coef = 4;
+
+	uint32_t throughput = 256*256*64*8;
+
+	static bool init[MAX_GPUS] = { 0 };
+	if (!init[thr_id])
+	{
+		cudaSetDevice(device_map[thr_id]); 
+		cudaDeviceReset();
+		cudaSetDeviceFlags(cudaDeviceScheduleBlockingSync);
+		cudaDeviceSetCacheConfig(cudaFuncCachePreferL1);
+		 
+		CUDA_SAFE_CALL(cudaMalloc(&d_hash[thr_id],  8 * sizeof(uint32_t) * throughput));
+		bitcredit_cpu_init(thr_id, throughput, d_hash[thr_id]);
+		init[thr_id] = true;
+	}
+
+	uint32_t endiandata[42],endianmid[8];
+		for (int k = 0; k < 42; k++)
+			be32enc(&endiandata[k], ((uint32_t*)pdata)[k]);
+
+	bitcredit_setBlockTarget(pdata,midstate,ptarget);
+	uint64_t nloop = max_nonce/throughput + 1;
+	do {
+		int order = 0;
+		uint32_t foundNonce = bitcredit_cpu_hash(thr_id, throughput, pdata[35], order++);
+		if  (foundNonce != 0xffffffff)
+		{
+				//if (opt_benchmark)
+				//	applog(LOG_INFO, "GPU #%d Found nounce %08x", thr_id, foundNonce);
+
+				pdata[35] = foundNonce;
+				*hashes_done = foundNonce - first_nonce + 1;
+				return 1;
+
+		}
+		if ((uint64_t)pdata[35] + throughput >(uint64_t)0xffffffff) {
+                       pdata[35]=0xffffffff; 
+                      *hashes_done = pdata[35] - first_nonce + 1; return 0;
+        } else { 
+    
+		pdata[35] += throughput;}
+	} while (pdata[19] < max_nonce && !work_restart[thr_id].restart);
+	*hashes_done = pdata[35] - first_nonce + 1;
+	return 0;
+}

bitcredit/cuda_bitcredit.cu
@@ -1,0 +1,273 @@
+
+/*
+#include <cuda.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+
+
+#include <stdint.h>
+#include <memory.h>
+*/
+#include <stdio.h>
+#include <memory.h>
+#include "cuda_vector.h" 
+
+extern cudaError_t MyStreamSynchronize(cudaStream_t stream, int situation, int thr_id);
+
+// __device__  uint4 *  S;
+ __device__ uint8 *TheHash;
+uint32_t *d_BitNonce[MAX_GPUS];
+__constant__  uint32_t pTarget[8];
+__constant__  uint8 midstate;
+__constant__  uint32_t  c_data[48];
+__constant__  uint16 shapad;
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// sha256 Transform function /////////////////////////
+
+
+
+static __constant__ uint8 pad4 =
+{
+	0x80000000, 0x00000000, 0x00000000, 0x00000000,
+	0x00000000, 0x00000000, 0x00000000, 0x00000100
+};
+
+
+
+static __constant__  uint8 H256 = {
+	0x6A09E667, 0xBB67AE85, 0x3C6EF372,
+	0xA54FF53A, 0x510E527F, 0x9B05688C,
+	0x1F83D9AB, 0x5BE0CD19
+};
+
+static  __constant__  uint32_t Ksha[64] = {
+	0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
+	0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
+	0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
+	0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174,
+	0xE49B69C1, 0xEFBE4786, 0x0FC19DC6, 0x240CA1CC,
+	0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
+	0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7,
+	0xC6E00BF3, 0xD5A79147, 0x06CA6351, 0x14292967,
+	0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13,
+	0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85,
+	0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3,
+	0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
+	0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5,
+	0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3,
+	0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
+	0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2
+};
+
+static __device__ __forceinline__ uint32_t bsg2_0(const uint32_t x)
+{
+	uint32_t r1 = ROTR32(x, 2);
+	uint32_t r2 = ROTR32(x, 13);
+	uint32_t r3 = ROTR32(x, 22);
+	return xor3b(r1, r2, r3);
+}
+
+
+static __device__ __forceinline__ uint32_t bsg2_1(const uint32_t x)
+{
+	uint32_t r1 = ROTR32(x, 6);
+	uint32_t r2 = ROTR32(x, 11);
+	uint32_t r3 = ROTR32(x, 25);
+	return xor3b(r1, r2, r3);
+}
+
+static __device__ __forceinline__ uint32_t ssg2_0(const uint32_t x)
+{
+	uint32_t r1 = ROTR32(x, 7);
+	uint32_t r2 = ROTR32(x, 18);
+	uint32_t r3 = shr_t32(x, 3);
+	return xor3b(r1, r2, r3);
+}
+
+static __device__ __forceinline__ uint32_t ssg2_1(const uint32_t x)
+{
+	uint32_t r1 = ROTR32(x, 17);
+	uint32_t r2 = ROTR32(x, 19);
+	uint32_t r3 = shr_t32(x, 10);
+	return xor3b(r1, r2, r3);
+}
+
+static __device__ __forceinline__ void sha2_step1(const uint32_t a, const uint32_t b, const uint32_t c, uint32_t &d, const uint32_t e,
+	const uint32_t f, const uint32_t g, uint32_t &h, const uint32_t in, const uint32_t Kshared)
+{
+	uint32_t t1, t2;
+	uint32_t vxandx = xandx(e, f, g);
+	uint32_t bsg21 = bsg2_1(e);
+	uint32_t bsg20 = bsg2_0(a);
+	uint32_t andorv = andor32(a, b, c);
+
+	t1 = h + bsg21 + vxandx + Kshared + in;
+	t2 = bsg20 + andorv;
+	d = d + t1;
+	h = t1 + t2;
+}
+
+static __device__ __forceinline__ void sha2_step2(const uint32_t a, const uint32_t b, const uint32_t c, uint32_t &d, const uint32_t e,
+	const uint32_t f, const uint32_t g, uint32_t &h, uint32_t* in, const uint32_t pc, const uint32_t Kshared)
+{
+	uint32_t t1, t2;
+
+	int pcidx1 = (pc - 2) & 0xF;
+	int pcidx2 = (pc - 7) & 0xF;
+	int pcidx3 = (pc - 15) & 0xF;
+	uint32_t inx0 = in[pc];
+	uint32_t inx1 = in[pcidx1];
+	uint32_t inx2 = in[pcidx2];
+	uint32_t inx3 = in[pcidx3];
+
+	uint32_t ssg21 = ssg2_1(inx1);
+	uint32_t ssg20 = ssg2_0(inx3);
+	uint32_t vxandx = xandx(e, f, g);
+	uint32_t bsg21 = bsg2_1(e);
+	uint32_t bsg20 = bsg2_0(a);
+	uint32_t andorv = andor32(a, b, c);
+
+	in[pc] = ssg21 + inx2 + ssg20 + inx0;
+
+	t1 = h + bsg21 + vxandx + Kshared + in[pc];
+	t2 = bsg20 + andorv;
+	d = d + t1;
+	h = t1 + t2;
+}
+
+
+
+
+static __device__ __forceinline__
+uint8 sha256_Transform2(uint16 in[1], const uint8 &r) // also known as sha2_round_body
+{
+	uint8 tmp = r;
+#define a  tmp.s0
+#define b  tmp.s1
+#define c  tmp.s2
+#define d  tmp.s3
+#define e  tmp.s4
+#define f  tmp.s5
+#define g  tmp.s6
+#define h  tmp.s7
+
+	sha2_step1(a, b, c, d, e, f, g, h, in[0].s0, Ksha[0]);
+	sha2_step1(h, a, b, c, d, e, f, g, in[0].s1, Ksha[1]);
+	sha2_step1(g, h, a, b, c, d, e, f, in[0].s2, Ksha[2]);
+	sha2_step1(f, g, h, a, b, c, d, e, in[0].s3, Ksha[3]);
+	sha2_step1(e, f, g, h, a, b, c, d, in[0].s4, Ksha[4]);
+	sha2_step1(d, e, f, g, h, a, b, c, in[0].s5, Ksha[5]);
+	sha2_step1(c, d, e, f, g, h, a, b, in[0].s6, Ksha[6]);
+	sha2_step1(b, c, d, e, f, g, h, a, in[0].s7, Ksha[7]);
+	sha2_step1(a, b, c, d, e, f, g, h, in[0].s8, Ksha[8]);
+	sha2_step1(h, a, b, c, d, e, f, g, in[0].s9, Ksha[9]);
+	sha2_step1(g, h, a, b, c, d, e, f, in[0].sa, Ksha[10]);
+	sha2_step1(f, g, h, a, b, c, d, e, in[0].sb, Ksha[11]);
+	sha2_step1(e, f, g, h, a, b, c, d, in[0].sc, Ksha[12]);
+	sha2_step1(d, e, f, g, h, a, b, c, in[0].sd, Ksha[13]);
+	sha2_step1(c, d, e, f, g, h, a, b, in[0].se, Ksha[14]);
+	sha2_step1(b, c, d, e, f, g, h, a, in[0].sf, Ksha[15]);
+
+#pragma unroll 3
+	for (int i = 0; i<3; i++) {
+
+		sha2_step2(a, b, c, d, e, f, g, h, (uint32_t*)in, 0, Ksha[16 + 16 * i]);
+		sha2_step2(h, a, b, c, d, e, f, g, (uint32_t*)in, 1, Ksha[17 + 16 * i]);
+		sha2_step2(g, h, a, b, c, d, e, f, (uint32_t*)in, 2, Ksha[18 + 16 * i]);
+		sha2_step2(f, g, h, a, b, c, d, e, (uint32_t*)in, 3, Ksha[19 + 16 * i]);
+		sha2_step2(e, f, g, h, a, b, c, d, (uint32_t*)in, 4, Ksha[20 + 16 * i]);
+		sha2_step2(d, e, f, g, h, a, b, c, (uint32_t*)in, 5, Ksha[21 + 16 * i]);
+		sha2_step2(c, d, e, f, g, h, a, b, (uint32_t*)in, 6, Ksha[22 + 16 * i]);
+		sha2_step2(b, c, d, e, f, g, h, a, (uint32_t*)in, 7, Ksha[23 + 16 * i]);
+		sha2_step2(a, b, c, d, e, f, g, h, (uint32_t*)in, 8, Ksha[24 + 16 * i]);
+		sha2_step2(h, a, b, c, d, e, f, g, (uint32_t*)in, 9, Ksha[25 + 16 * i]);
+		sha2_step2(g, h, a, b, c, d, e, f, (uint32_t*)in, 10, Ksha[26 + 16 * i]);
+		sha2_step2(f, g, h, a, b, c, d, e, (uint32_t*)in, 11, Ksha[27 + 16 * i]);
+		sha2_step2(e, f, g, h, a, b, c, d, (uint32_t*)in, 12, Ksha[28 + 16 * i]);
+		sha2_step2(d, e, f, g, h, a, b, c, (uint32_t*)in, 13, Ksha[29 + 16 * i]);
+		sha2_step2(c, d, e, f, g, h, a, b, (uint32_t*)in, 14, Ksha[30 + 16 * i]);
+		sha2_step2(b, c, d, e, f, g, h, a, (uint32_t*)in, 15, Ksha[31 + 16 * i]);
+
+	}
+#undef a
+#undef b
+#undef c
+#undef d
+#undef e
+#undef f
+	return (r + tmp);
+}
+
+
+
+
+
+__global__ __launch_bounds__(256, 3) void bitcredit_gpu_hash(int threads, uint32_t startNonce,uint32_t* NonceVector)
+{
+
+	int thread = (blockDim.x * blockIdx.x + threadIdx.x);
+	
+	uint32_t nonce = startNonce + thread;
+	
+	uint16 data[1]; 
+    uint8 state,state2;
+
+	data[0] = ((uint16*)c_data)[2];
+	data[0].s3 = nonce;
+	state = sha256_Transform2(data, midstate);
+
+	data[0].lo=state;
+	data[0].hi=pad4;
+
+	state2 = sha256_Transform2(data, H256);
+	if (MAKE_ULONGLONG(cuda_swab32(state2.s6), cuda_swab32(state2.s7)) <= ((uint64_t*)pTarget)[3]){ NonceVector[0] = nonce; }
+
+}
+
+
+void bitcredit_cpu_init(uint32_t thr_id, int threads, uint32_t *hash)
+{
+    
+	cudaMemcpyToSymbol(TheHash, &hash, sizeof(hash), 0, cudaMemcpyHostToDevice);
+	cudaMalloc(&d_BitNonce[thr_id], sizeof(uint32_t)); 
+	
+} 
+
+
+__host__ uint32_t bitcredit_cpu_hash(uint32_t thr_id, int threads, uint32_t startNounce,  int order)
+{
+	uint32_t result[MAX_GPUS] = {0xffffffff};
+	cudaMemset(d_BitNonce[thr_id], 0xffffffff, sizeof(uint32_t));
+
+ 
+	const int threadsperblock = 256;
+	 	
+	dim3 grid((threads + threadsperblock - 1) / threadsperblock);
+	dim3 block(threadsperblock);
+	
+	bitcredit_gpu_hash << <grid, block >> >(threads, startNounce, d_BitNonce[thr_id]);
+
+	cudaMemcpy(&result[thr_id], d_BitNonce[thr_id], sizeof(uint32_t), cudaMemcpyDeviceToHost);
+	
+return result[thr_id];
+}
+
+__host__ void bitcredit_setBlockTarget(uint32_t* pdata, const uint32_t* zmidstate,const void *target)
+{
+
+		unsigned char PaddedMessage[192]; //bring balance to the force
+		memcpy(PaddedMessage,     pdata, 168);
+		memset(PaddedMessage+168, 0, 24);
+		((uint32_t*)PaddedMessage)[42]=0x80000000;
+		((uint32_t*)PaddedMessage)[47]=0x0540;
+	
+		CUDA_SAFE_CALL(cudaMemcpyToSymbol(midstate, zmidstate, 8 * sizeof(uint32_t), 0, cudaMemcpyHostToDevice));
+		CUDA_SAFE_CALL(cudaMemcpyToSymbol(pTarget, target, 8 * sizeof(uint32_t), 0, cudaMemcpyHostToDevice));
+		CUDA_SAFE_CALL(cudaMemcpyToSymbol(c_data, PaddedMessage, 48 * sizeof(uint32_t), 0, cudaMemcpyHostToDevice));
+}
+

ccminer.vcxproj
@@ -250,6 +250,7 @@
     </CudaCompile>
     <ClCompile Include="sph\neoscrypt.c" />
     <ClCompile Include="sph\sha256_Y.c" />
+    <ClCompile Include="sph\sph_sha2.c" />
     <ClCompile Include="sph\yescrypt-opt.c" />
     <ClCompile Include="sph\yescryptcommon.c" />
     <ClCompile Include="scrypt-jane.cpp" />
@@ -321,7 +322,7 @@
     <ClInclude Include="miner.h" />
     <ClInclude Include="nvml.h" />
     <ClInclude Include="scrypt\salsa_kernel.h" />
-	<ClInclude Include="sph\neoscrypt.h" />
+    <ClInclude Include="sph\neoscrypt.h" />
     <ClInclude Include="sph\sha256_Y.h" />
     <ClInclude Include="sph\sph_blake.h" />
     <ClInclude Include="sph\sph_bmw.h" />
@@ -341,7 +342,7 @@
     <ClInclude Include="sph\sph_hamsi.h" />
     <ClInclude Include="sph\sph_types.h" />
     <ClInclude Include="sph\sph_whirlpool.h" />
-	<ClInclude Include="sph\sysendian.h" />
+    <ClInclude Include="sph\sysendian.h" />
     <ClInclude Include="sph\yescrypt.h" />
     <ClInclude Include="uint256.h" />
   </ItemGroup>
@@ -350,6 +351,8 @@
     <ClInclude Include="lyra2\Sponge.h" />
   </ItemGroup>
   <ItemGroup>
+    <CudaCompile Include="bitcredit\bitcredit.cu" />
+    <CudaCompile Include="bitcredit\cuda_bitcredit.cu" />
     <CudaCompile Include="cuda.cpp" />
     <CudaCompile Include="bitslice_transformations_quad.cu">
       <ExcludedFromBuild>true</ExcludedFromBuild>
@@ -408,7 +411,7 @@
       <MaxRegCount>92</MaxRegCount>
     </CudaCompile>
     <CudaCompile Include="Algo256\cuda_skein256.cu" />
-	<CudaCompile Include="neoscrypt\cuda_neoscrypt.cu" />
+    <CudaCompile Include="neoscrypt\cuda_neoscrypt.cu" />
     <CudaCompile Include="neoscrypt\neoscrypt.cu" />
     <CudaCompile Include="pentablake.cu">
       <MaxRegCount>80</MaxRegCount>
@@ -506,7 +509,7 @@
     </CudaCompile>
     <CudaCompile Include="x17\x17.cu">
     </CudaCompile>
-	<CudaCompile Include="yescrypt\cuda_yescrypt.cu" />
+    <CudaCompile Include="yescrypt\cuda_yescrypt.cu" />
     <CudaCompile Include="yescrypt\yescrypt.cu" />
   </ItemGroup>
   <Import Project="$(VCTargetsPath)\Microsoft.Cpp.targets" />

ccminer.vcxproj.filters
@@ -221,6 +221,13 @@
     <ClCompile Include="api.cpp">
       <Filter>Source Files\CUDA</Filter>
     </ClCompile>
+    <ClCompile Include="sph\neoscrypt.c" />
+    <ClCompile Include="sph\sha256_Y.c" />
+    <ClCompile Include="sph\yescrypt-opt.c" />
+    <ClCompile Include="sph\yescryptcommon.c" />
+    <ClCompile Include="sph\sph_sha2.c">
+      <Filter>Source Files\sph</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="compat.h">
@@ -362,6 +369,10 @@
       <Filter>Header Files\lyra2</Filter>
     </ClInclude>
     <ClInclude Include="scrypt\salsa_kernel.h" />
+    <ClInclude Include="sph\neoscrypt.h" />
+    <ClInclude Include="sph\sha256_Y.h" />
+    <ClInclude Include="sph\sysendian.h" />
+    <ClInclude Include="sph\yescrypt.h" />
   </ItemGroup>
   <ItemGroup>
     <CudaCompile Include="cuda.cpp">
@@ -567,5 +578,11 @@
     <CudaCompile Include="scrypt\salsa_kernel.cu" />
     <CudaCompile Include="scrypt\sha256.cu" />
     <CudaCompile Include="scrypt\titan_kernel.cu" />
+    <CudaCompile Include="bitcredit\bitcredit.cu" />
+    <CudaCompile Include="bitcredit\cuda_bitcredit.cu" />
+    <CudaCompile Include="neoscrypt\cuda_neoscrypt.cu" />
+    <CudaCompile Include="neoscrypt\neoscrypt.cu" />
+    <CudaCompile Include="yescrypt\cuda_yescrypt.cu" />
+    <CudaCompile Include="yescrypt\yescrypt.cu" />
   </ItemGroup>
 </Project>

miner.h
@@ -404,6 +404,10 @@ extern int scanhash_yescrypt(int thr_id, uint32_t *pdata,
 	const uint32_t *ptarget, uint32_t max_nonce,
 	unsigned long *hashes_done);
 
+extern int scanhash_bitcredit(int thr_id, uint32_t *pdata,
+	const uint32_t *ptarget, const uint32_t *midstate, uint32_t max_nonce,
+	unsigned long *hashes_done);
+
 /* api related */
 void *api_thread(void *userdata);
 void api_set_throughput(int thr_id, uint32_t throughput);
@@ -612,10 +616,10 @@ struct stratum_ctx {
 };
 
 struct work {
-	uint32_t data[32];
+	uint32_t data[64]; //42 needed for bitcredit
 	uint32_t target[8];
 	uint32_t maxvote;
-
+	uint32_t midstate[8];
 	char job_id[128];
 	size_t xnonce2_len;
 	uchar xnonce2[32];

commit 65aaa104ebbce667b49c80f83172fc65dbe22479

Ensure data string is correctly formatted for solo work submission
This should fix solo mining submission which was broken in 97a9ef1
when data was expanded from 32 to 64 uint32_ts in struct work.

ccminer.cpp
@@ -770,7 +770,21 @@ static bool submit_upstream_work(CURL *curl, struct work *work)
 
 		/* build hex string */
 		char *str = NULL;
-		int data_size = ((opt_algo == ALGO_NEO) ) ? 80 : sizeof(work->data);
+		int data_size;
+
+		switch (opt_algo)
+		{
+		case ALGO_NEO:
+			data_size = 80;
+			break;
+		case ALGO_BITC:
+			data_size = 168;
+			break;
+		default:
+			data_size = 128;
+			break;
+		}
+
 		if (opt_algo != ALGO_HEAVY && opt_algo != ALGO_MJOLLNIR) {
 			for (int i = 0; i < (data_size >> 2); i++)
 				le32enc(work->data + i, work->data[i]);