4b7ffdc1a20a0b9f431cac65e05b8b5cf50d4a8d
sp-hash <rune.stensland@bradyplc.com>
2015-11-04 19:42:36 +0100

    Improved the reject rate in the skein algo.

--- skein.cu
+++ skein.cu
@@ -48,9 +48,9 @@ int scanhash_skeincoin(int thr_id, uint32_t *pdata,
 								  uint32_t *hashes_done)
 {
 	const uint32_t first_nonce = pdata[19];
-	const int swap = 1;
+	const bool swap = true;
 
-	uint32_t intensity = (device_sm[device_map[thr_id]] > 500) ? 1 << 28 : 1 << 27;;
+	uint32_t intensity = (device_sm[device_map[thr_id]] > 500) ? 1 << 25 : 1 << 24;;
 	uint32_t throughput = device_intensity(device_map[thr_id], __func__, intensity); // 256*4096
 	throughput = min(throughput, max_nonce - first_nonce);
 
@@ -58,7 +58,7 @@ int scanhash_skeincoin(int thr_id, uint32_t *pdata,
 	{
 		((uint64_t*)ptarget)[3] = 0x3000f0000;
 	}
-	uint64_t target = ((uint64_t*)ptarget)[3];
+//	uint64_t target = ((uint64_t*)ptarget)[3];
 	if (!init[thr_id])
 	{
 		CUDA_SAFE_CALL(cudaSetDevice(device_map[thr_id]));
@@ -79,13 +79,13 @@ int scanhash_skeincoin(int thr_id, uint32_t *pdata,
 	skein512_cpu_setBlock_80(thr_id, (void*)endiandata);
 	do
 	{
-
+		if(scan_abort_flag || work_restart[thr_id].restart) return 0;
 		if (device_sm[device_map[thr_id]] > 500)
-			skein512_cpu_hash_80_52(thr_id, throughput, pdata[19], swap, target, foundnonces[thr_id]);
+			skein512_cpu_hash_80_52(thr_id, throughput, pdata[19], swap, ((uint64_t*)ptarget)[3], foundnonces[thr_id]);
 		else
-			skein512_cpu_hash_80_50(thr_id, throughput, pdata[19], swap, target, foundnonces[thr_id]);
-
-		if (foundnonces[thr_id][0] != 0xffffffff)
+			skein512_cpu_hash_80_50(thr_id, throughput, pdata[19], swap, ((uint64_t*)ptarget)[3], foundnonces[thr_id]);
+		
+		if (foundnonces[thr_id][0] != 0xffffffff && (!scan_abort_flag || !work_restart[thr_id].restart))
 		{
 			uint32_t vhash64[8];
 
@@ -94,10 +94,10 @@ int scanhash_skeincoin(int thr_id, uint32_t *pdata,
 			skeincoinhash(vhash64, endiandata);
 
 			uint64_t test = ((uint64_t*)vhash64)[3];
-			if (test <= target && fulltest(vhash64, ptarget))
+			if (test <= ((uint64_t*)ptarget)[3] && fulltest(vhash64, ptarget))
 			{
 				int res = 1;
-				*hashes_done = pdata[19] - first_nonce + throughput+1;
+				*hashes_done = pdata[19] - first_nonce + throughput;
 
 				if (opt_debug || opt_benchmark)
 					applog(LOG_INFO, "GPU #%d: found nonce $%08X", thr_id, foundnonces[thr_id][0]);
@@ -106,16 +106,16 @@ int scanhash_skeincoin(int thr_id, uint32_t *pdata,
 					endiandata[19] = swab32_if(foundnonces[thr_id][1], swap);
 					skeincoinhash(vhash64, endiandata);
 					uint64_t test2 = ((uint64_t*)vhash64)[3];
-					if (test2 <= target && fulltest(vhash64, ptarget))
+					if (test2 <= ((uint64_t*)ptarget)[3] && fulltest(vhash64, ptarget))
 					{
 						if (opt_debug || opt_benchmark)
 							applog(LOG_INFO, "GPU #%d: found second nonce $%08X", thr_id, foundnonces[thr_id][1]);
-						pdata[19 + res] = swab32_if(foundnonces[thr_id][1], !swap);
+						pdata[21] = swab32_if(foundnonces[thr_id][1], !swap);
 						res++;
 					}
 					else
 					{
-						if (test2 != target) applog(LOG_WARNING, "GPU #%d: result for nonce $%08X does not validate on CPU!", thr_id, foundnonces[thr_id][1]);
+						if (test2 != ((uint64_t*)ptarget)[3]) applog(LOG_WARNING, "GPU #%d: result for second nonce $%08X does not validate on CPU!", thr_id, foundnonces[thr_id][1]);
 					}
 				}
 				pdata[19] = swab32_if(foundnonces[thr_id][0], !swap);
@@ -123,7 +123,7 @@ int scanhash_skeincoin(int thr_id, uint32_t *pdata,
 			}
 			else 
 			{
-				if (test != target)
+				if (test != ((uint64_t*)ptarget)[3])
 					applog(LOG_WARNING, "GPU #%d: result for nonce $%08X does not validate on CPU!", thr_id, foundnonces[thr_id][0]);
 				else
 					applog(LOG_WARNING, "Lost work: #%d", test);
